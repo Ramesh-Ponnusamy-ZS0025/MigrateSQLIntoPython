87256818bb80d5c3fe42075fe28fd800a8e60882
Ramesh Ponnusamy <ramesh.p@zucisystems.com>
2024-10-09 13:31:23 +0530

    fine tuned QA prompt

--- app/common.py
+++ app/common.py
@@ -6,3 +6,16 @@ def extract_code(text,language):
     code_blocks = re.findall(pattern, text, re.DOTALL)
     code_blocks = ''.join(code_blocks)
     return code_blocks
+
+def extract_java_code(data):
+    # Regular expression to extract Java code blocks from markdown-style code blocks (```)
+    java_code_pattern = r'```(java)?\n(.*?)```'
+    java_code_matches = re.findall(java_code_pattern, data, re.DOTALL)
+    # mycode = ''.join(java_code_matches)
+    mycode = ''.join(match[1] for match in java_code_matches)
+
+    # Extracting and printing Java code from matches found in string data.
+    # for match in java_code_matches:
+    #     print(match.strip())
+
+    return mycode
--- app/qa.py
+++ app/qa.py
@@ -2,7 +2,7 @@
 
 from docx import Document
 from .groq_api import get_llm_response
-from .common import extract_code
+from .common import extract_code, extract_java_code
 from config import UPLOAD_FOLDER
 import os
 
@@ -36,11 +36,7 @@ def process_qa_llm(file_path,file_id):
             Generate a document containing BDD-style test cases derived from the acceptance criteria.
             add line "process_zuci_is_done"
             
-            Selenium Cucumber Java Test Automation Script:
-            Develop a complete Selenium Cucumber java  test automation complete script that includes:
-            A feature file reflecting the BDD test cases.
-            A step definition file that implements the necessary automation steps.  
-            add line "process_zuci_is_done" 
+            
             
             I will split the response using "process_zuci_is_done" finally from response
            """
@@ -52,7 +48,20 @@ def process_qa_llm(file_path,file_id):
     print(split_response)
     acceptance_criteria_content = split_response[0]
     bdd_style_test_case_content = split_response[1]
-    java_code = extract_code(split_response[2], 'java')
+    # java_code = extract_code(split_response[2], 'java')
+    # input_requirements = """" bdd_style_test_case_content"""
+    java_code_prompt = """ Input Requirements as BDD Style Test Cases  """+bdd_style_test_case_content+"""" 
+            Output Requirements:
+            From the given BDD Style Test Cases , please generate the  Selenium Cucumber Java Test Automation Script:
+            Develop a complete Selenium Cucumber java  test automation complete script that includes:
+            A step definition file that implements the necessary automation steps.  
+            return only java code no other contents
+             """
+
+    content_response = get_llm_response(java_code_prompt)
+
+    java_code = extract_code(content_response, 'java')
+    # java_code = extract_java_code(content_response)
     base_filename = os.path.splitext(os.path.basename(file_path))[0]
     base_filename +="_"+str(file_id)
     ac_file_path = base_filename + '_acceptance_criteria.txt'
